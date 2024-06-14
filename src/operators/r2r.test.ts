5ab96ef90ab183554a1d91d887784c1b5302afac
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-06-14 11:45:32 +0200

    feat: Add extension functions for sqrt and pow in R2ROperator with comunica

--- src/operators/r2r.test.ts
+++ src/operators/r2r.test.ts
@@ -1,11 +1,11 @@
-import {QuadContainer} from "./s2r";
+import { QuadContainer } from "./s2r";
 const N3 = require('n3');
 
 const { DataFactory } = N3;
 const { namedNode, literal, defaultGraph, quad } = DataFactory;
 // @ts-ignore
-import {Quad} from 'n3';
-import {R2ROperator} from "./r2r";
+import { Quad } from 'n3';
+import { R2ROperator } from "./r2r";
 
 test('test_query_engine', async () => {
     let r2r = new R2ROperator(`SELECT * WHERE { ?s ?p ?o }`);
@@ -24,7 +24,7 @@ test('test_query_engine', async () => {
     let quadSet = new Set<Quad>();
     quadSet.add(quad1);
     quadSet.add(quad2);
-    let container = new QuadContainer(quadSet,0);
+    let container = new QuadContainer(quadSet, 0);
     const bindingsStream = await r2r.execute(container);
     let resuults = new Array<string>();
     // @ts-ignore
@@ -38,4 +38,38 @@ test('test_query_engine', async () => {
         expect(resuults.length).toBe(2);
 
     });
+});
+
+test('test_query_engine_with_extension_functions', async () => {
+    let r2r = new R2ROperator(`
+        PREFIX extension: <http://extension.org/functions#>
+        SELECT (extension:sqrt(?o) as ?sqrt) (extension:pow(?o,2) as ?pow) WHERE { ?s ?p ?o }`);
+    const quad1 = quad(
+        namedNode('https://rsp.js/test_subject_0'),
+        namedNode('http://rsp.js/test_property'),
+        literal('4'),
+        defaultGraph(),
+    );
+    const quad2 = quad(
+        namedNode('https://rsp.js/test_subject_1'),
+        namedNode('http://rsp.js/test_property'),
+        literal('9'),
+        defaultGraph(),
+    );
+    let quadSet = new Set<Quad>();
+    quadSet.add(quad1);
+    quadSet.add(quad2);
+    let container = new QuadContainer(quadSet, 0);
+    const bindingsStream = await r2r.execute(container);
+    let results = new Array<string>();
+    // @ts-ignore
+    bindingsStream.on('data', (binding) => {
+        results.push(binding);
+    });
+
+    bindingsStream.on('end', () => {
+        // The data-listener will not be called anymore once we get here.
+        expect(results.length).toBe(2);
+    });
+
 });
--- src/operators/r2r.ts
+++ src/operators/r2r.ts
@@ -1,27 +1,26 @@
-import {QuadContainer} from "./s2r";
+import { QuadContainer } from "./s2r";
+import { DataFactory } from "rdf-data-factory";
 const N3 = require('n3');
-
-const { DataFactory } = N3;
-const { namedNode, literal, defaultGraph, quad } = DataFactory;
+const DF = new DataFactory();
 // @ts-ignore
-import {Quad} from 'n3';
-export class R2ROperator{
+import { Literal, Quad } from 'n3';
+export class R2ROperator {
     query: string;
-    staticData : Set<Quad>;
-    constructor(query: string){
+    staticData: Set<Quad>;
+    constructor(query: string) {
         this.query = query;
         this.staticData = new Set<Quad>();
     }
-    addStaticData(quad: Quad){
+    addStaticData(quad: Quad) {
         this.staticData.add(quad);
     }
-    async execute(container: QuadContainer){
+    async execute(container: QuadContainer) {
         const store = new N3.Store();
-        for(let elem of container.elements){
+        for (let elem of container.elements) {
             store.addQuad(elem);
 
         }
-        for(let elem of this.staticData){
+        for (let elem of this.staticData) {
             store.addQuad(elem);
         }
         const QueryEngine = require('@comunica/query-sparql').QueryEngine;
@@ -29,6 +28,23 @@ export class R2ROperator{
         const myEngine = new QueryEngine();
         return await myEngine.queryBindings(this.query, {
             sources: [store],
+            extensionFunctions: {
+                'http://extension.org/functions#sqrt'(args: any) {
+                    const arg = args[0];
+                    if (arg.termType === 'Literal') {
+                        return DF.literal(Math.sqrt(Number(arg.value)).toString());
+                    }
+                },
+                'http://extension.org/functions#pow'(args: any) {
+                    const arg1 = args[0];
+                    if (arg1.termType === 'Literal') {
+                        const arg2 = args[1];
+                        if (arg2.termType === 'Literal') {
+                            return DF.literal(Math.pow(Number(arg1.value), Number(arg2.value)).toString());
+                        }
+                    }
+                }
+            },
         });
     }
 }
